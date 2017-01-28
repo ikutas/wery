62748c7f2931aff18c74576c694b2efec155cbbd
nabettu <tatsuaki.w@gmail.com>
2017-01-29 03:17:28 +0900

    index post整理

--- index.php
+++ index.php
@@ -1,76 +1,55 @@
 <?php get_header(); ?>
-
 			<div id="content">
-
 				<div id="inner-content" class="wrap cf">
-
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+						<main id="main" class="index__main m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+							<?php
+							$postTitle = get_the_title();
+							$thumbUrl = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' )["0"];
+							foreach ((get_the_category()) as $cat) {
+							    $postCatId = $cat->cat_ID;
+							    break;
+							}
+							$categoryLink = get_category_link($postCatId);
+							?>
 
-								<header class="article-header">
-
-									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<p class="byline entry-meta vcard">
-                                                                        <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-                       								/* the time the post was published */
-                       								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       								/* the author of the post */
-                       								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    							); ?>
-									</p>
-
-								</header>
-
-								<section class="entry-content cf">
-									<?php the_content(); ?>
-								</section>
-
-								<footer class="article-footer cf">
-									<p class="footer-comment-count">
-										<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
-									</p>
-
-
-                 	<?php printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
-
-                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-
-								</footer>
-
-							</article>
+							<div id="post-<?php the_ID(); ?>" <?php post_class(['cf','index__main__post']); ?>>
+								<a class="index__main__post__thumb" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>" style="background-image: url('<?php echo $thumbUrl;?>')">
+									<?php echo $postTitle; ?>
+								</a>
+								<a class="index__main__post__title" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
+									<h3 class="h2 entry-title">
+											<?php the_title(); ?>
+									</h3>
+								</a>
+								<p class="byline entry-meta vcard index__main__post__date">
+                     <time class="updated entry-time" datetime="<?php echo get_the_time('Y-m-d') ?>" itemprop="datePublished"><?php echo get_the_time('Y/m/d') ?></time>
+								</p>
+								<a class="index__main__post__category" href="<?php echo $categoryLink; ?>" title="<?php echo $cat->cat_name; ?>"> <?php echo $cat->cat_name; ?></a>
+								<div class="index__main__post__tagBox">
+									<?php the_tags('', ''); ?>
+								</div>
+							</div>
 
 							<?php endwhile; ?>
-
 									<?php bones_page_navi(); ?>
-
 							<?php else : ?>
-
 									<article id="post-not-found" class="hentry cf">
 											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+												<h1><?php _e('Oops, Post Not Found!', 'bonestheme'); ?></h1>
 										</header>
 											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+												<p><?php _e('Uh Oh. Something is missing. Try double checking things.', 'bonestheme'); ?></p>
 										</section>
 										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+												<p><?php _e('This is the error message in the index.php template.', 'bonestheme'); ?></p>
 										</footer>
 									</article>
-
 							<?php endif; ?>
-
-
 						</main>
-
 					<?php get_sidebar(); ?>
-
 				</div>
-
 			</div>
-
-
 <?php get_footer(); ?>
